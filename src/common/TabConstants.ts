64bd4b1631709683c80fc9df7b0a0f7ce5d34817
Sumit Kanchan <sumit.kanchan04@live.com>
2019-08-08 10:16:51 +0530

    Updated the constants for displaying the text

--- src/common/TabConstants.ts
+++ src/common/TabConstants.ts
@@ -1,7 +1,7 @@
 export default class TabConstants {
 
     public static ADD_TAB_GUID: string = 'e91cd72e-4c38-4229-a4b6-bd6045207d72';
-    public static NO_TAB_ADDED:string = `<p>No tab added to the webpart. Click on '+' button to add a new tab and content...</p>`
+    public static NO_TAB_ADDED_EDIT_MODE:string = `<p>No tab added to the webpart. Click on '+' button to add a new tab and content...</p>`;
+    public static NO_TAB_ADDED_VIEW_MODE:string = `<p>No tab content added to the webpart. Edit the webpart to add a new tab content...</p>`;
 }
-
-export { TabConstants }
+export { TabConstants };
